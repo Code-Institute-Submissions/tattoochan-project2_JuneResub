96a643eb27bb75ef5069dd997b56b8eddfe80062
tattoochan <54732164+tattoochan@users.noreply.github.com>
2020-02-04 20:15:52 +0800

    sepererated html to multipls files.

---
+++ script.js
@@ -0,0 +1,53 @@
+function convertCurrency() {
+    // document.getElementsByClassName("cur").innerHTML = "";
+    var div = document.getElementById("currency-holder");
+    var y = div.getElementsByTagName("P");
+    var ctr;
+    for (ctr = 0; ctr < y.length; ctr++) {
+        y[ctr].innerHTML = "";
+    }
+    var from = document.getElementById("from").value;
+    var to = document.getElementById("to").value;
+    var xmlhttp = new XMLHttpRequest();
+    var url = "https://api.exchangerate-api.com/v4/latest/" + from;
+    xmlhttp.open("GET", url, true);
+    xmlhttp.send();
+    xmlhttp.onreadystatechange = function () {
+        if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+            var result = xmlhttp.responseText;
+            var jsResult = JSON.parse(result);
+            var oneUnit = jsResult.rates[to] / jsResult.rates[from];
+            for (x in jsResult.rates) {
+                if (x == 'USD') {
+                    document.getElementById("USA").innerHTML += jsResult.rates[x] + ' ' + x + "<br>"
+                }
+                if (x == 'SGD') {
+                    document.getElementById("SGD").innerHTML += jsResult.rates[x] + ' ' + x + "<br>"
+                }
+                if (x == 'EUR') {
+                    document.getElementById("EUR").innerHTML += jsResult.rates[x] + ' ' + x + "<br>"
+                }
+                if (x == 'GBP') {
+                    document.getElementById("GBP").innerHTML += jsResult.rates[x] + ' ' + x + "<br>"
+                }
+                if (x == 'INR') {
+                    document.getElementById("INR").innerHTML += jsResult.rates[x] + ' ' + x + "<br>"
+                }
+                if (x == 'AUD') {
+                    document.getElementById("AUD").innerHTML += jsResult.rates[x] + ' ' + x + "<br>"
+                }
+                if (x == 'PHP') {
+                    document.getElementById("PHP").innerHTML += jsResult.rates[x] + ' ' + x + "<br>"
+                }
+
+            }
+            var amt = document.getElementById("fromAmount").value;
+            var to_prefix = document.getElementById("to").value;
+            document.getElementById("toFromtxt").innerHTML = amt + ' ' + from + ' = ';
+            document.getElementById("toAmount").innerHTML = (oneUnit * amt).toFixed(3) + ' ' + to_prefix;
+
+
+        }
+    }
+
+}

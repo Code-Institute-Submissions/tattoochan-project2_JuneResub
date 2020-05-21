2424203aa325b7e76206f03f95448f7f342c7ece
Ubuntu <vincentchan@imba.com.sg>
2020-05-21 17:44:11 +0000

    Updated stylesheet with comments and remove unused style settings

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,4 @@
 function convertCurrency() {
-    // document.getElementsByClassName("cur").innerHTML = "";
     var div = document.getElementById("currency-holder");
     var y = div.getElementsByTagName("P");
     var ctr;
@@ -9,39 +8,45 @@ function convertCurrency() {
     var from = document.getElementById("from").value;
     var to = document.getElementById("to").value;
     var xmlhttp = new XMLHttpRequest();
+    // Setting the url for download
     var url = "https://api.exchangerate-api.com/v4/latest/" + from;
     xmlhttp.open("GET", url, true);
     xmlhttp.send();
+    
+    // download and process the data to display on the html page
     xmlhttp.onreadystatechange = function () {
         if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
             var result = xmlhttp.responseText;
             var jsResult = JSON.parse(result);
             var oneUnit = jsResult.rates[to] / jsResult.rates[from];
-            console.log(result)
+            
+            // display the rates for some prefix countries 
             for (let x in jsResult.rates) {
                 if (x == 'USD') {
-                    document.getElementById("USA").innerHTML += jsResult.rates[x] + ' ' + x + "<br>"
+                    document.getElementById("USA").innerHTML += jsResult.rates[x].toFixed(3) + ' ' + x + "<br>"
                 }
                 if (x == 'SGD') {
-                    document.getElementById("SGD").innerHTML += jsResult.rates[x] + ' ' + x + "<br>"
+                    document.getElementById("SGD").innerHTML += jsResult.rates[x].toFixed(3) + ' ' + x + "<br>"
                 }
                 if (x == 'EUR') {
-                    document.getElementById("EUR").innerHTML += jsResult.rates[x] + ' ' + x + "<br>"
+                    document.getElementById("EUR").innerHTML += jsResult.rates[x].toFixed(3) + ' ' + x + "<br>"
                 }
                 if (x == 'GBP') {
-                    document.getElementById("GBP").innerHTML += jsResult.rates[x] + ' ' + x + "<br>"
+                    document.getElementById("GBP").innerHTML += jsResult.rates[x].toFixed(3) + ' ' + x + "<br>"
                 }
                 if (x == 'INR') {
-                    document.getElementById("INR").innerHTML += jsResult.rates[x] + ' ' + x + "<br>"
+                    document.getElementById("INR").innerHTML += jsResult.rates[x].toFixed(3) + ' ' + x + "<br>"
                 }
                 if (x == 'AUD') {
-                    document.getElementById("AUD").innerHTML += jsResult.rates[x] + ' ' + x + "<br>"
+                    document.getElementById("AUD").innerHTML += jsResult.rates[x].toFixed(3) + ' ' + x + "<br>"
                 }
                 if (x == 'PHP') {
-                    document.getElementById("PHP").innerHTML += jsResult.rates[x] + ' ' + x + "<br>"
+                    document.getElementById("PHP").innerHTML += jsResult.rates[x].toFixed(3) + ' ' + x + "<br>"
                 }
 
             }
+            
+            // display result of selected currency conversion
             var amt = document.getElementById("fromAmount").value;
             var to_prefix = document.getElementById("to").value;
             document.getElementById("toFromtxt").innerHTML = amt + ' ' + from + ' = ';
